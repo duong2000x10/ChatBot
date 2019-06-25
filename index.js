811b3e6e32d98a5d972b6c450b7c0596456a189c
duong2000x10 <51240412+duong2000x10@users.noreply.github.com>
2019-06-25 17:12:58 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+// Imports the Google Cloud client library
+var { translate } = require("google-translate-api-browser");
+// Instantiates a client
+
+app.get('/', function (req, res) {
+    console.log(__dirname);
+    res.sendFile('./index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('new-message', function (msg) {
+        translate(msg.data, { from: 'en', to: 'vi' }).then(res => {
+            io.emit('new-message',msg);
+            io.emit('new-message', {
+                data: res.text,
+                from: "bot",
+                id: socket.id
+            });
+        }).catch(err => {
+            console.error(err);
+        });
+    });
+});
+
+http.listen(port, function () {
+    console.log('listening on *:' + port);
+});
